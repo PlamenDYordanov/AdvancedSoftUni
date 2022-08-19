1116db8c80e42ccc71fced7d8a9d104b14794d38
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-08-19 18:22:09 +0300

    Advanced

--- Stack_Queues/Exercise/P04_BasicQueueOperation.java
+++ Stack_Queues/Exercise/P04_BasicQueueOperation.java
@@ -4,46 +4,47 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class P02_BasicStackOperation {
+public class P04_BasicQueueOperation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-
         int pushNumbers = numbers[0];
-        int popNumbers = numbers[1];
+        int pollNumbers = numbers[1];
         int number = numbers[2];
 
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
 
         for (int i = 0; i < pushNumbers; i++) {
             int digit = scanner.nextInt();
-            stack.push(digit);
+            queue.offer(digit);
+            
         }
-        for (int i = 0; i < popNumbers; i++) {
-            stack.pop();
+        for (int i = 0; i < pollNumbers; i++) {
+            queue.poll();
         }
-        if (stack.isEmpty()) {
+        if (queue.isEmpty()){
             System.out.println(0);
-        } else {
-            int minNumber = Integer.MAX_VALUE;
-            boolean flag = true;
-            for (Integer integer : stack) {
-                if (integer < minNumber) {
-                    minNumber = integer;
-                }
-                if (integer == number) {
+        }else {
+            boolean flag = false;
+            int min = Integer.MAX_VALUE;
+            for (Integer integer : queue) {
+                if (integer == number){
                     System.out.println("true");
-                    flag = false;
+                    flag = true;
                     break;
+                }else {
+                    if (integer<min){
+                        min = integer;
+                    }
                 }
             }
-            if (flag) {
-                System.out.println(minNumber);
+            if (!flag){
+                System.out.println(min);
             }
-        }
 
+        }
     }
 }
