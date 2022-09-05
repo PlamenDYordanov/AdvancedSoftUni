dea0be937120cdf62f8af12b6536836ed3805c38
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-09-05 13:31:08 +0300

    Advanced

--- FileStreams/Lab/P07_ListFiles.java
+++ FileStreams/Lab/P07_ListFiles.java
@@ -1,4 +1,22 @@
 package Advanced.FileStreams.Lab;
 
+import java.io.File;
+import java.nio.file.Files;
+
 public class P07_ListFiles {
+    public static void main(String[] args) {
+        File file = new File("E:\\Advanced_SoftUni\\src\\Advanced\\FileStreams\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        if (file.exists()){
+            if (file.isDirectory()){
+                File[] files = file.listFiles();
+                for (File f : files) {
+                    if (!f.isDirectory()){
+                      
+                        System.out.printf("%s: [%s]%n", f.getName(), f.length());
+                    }
+                }
+            }
+        }
+    }
 }
