f63bba5cfca6040ad1770b38083e7ab70170f81b
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-09-15 21:51:48 +0300

    Advanced

--- DefiningClasses/BankAccount/BankAccount.java
+++ DefiningClasses/BankAccount/BankAccount.java
@@ -1,4 +1,42 @@
 package Advanced.DefiningClasses.BankAccount;
 
 public class BankAccount {
+    private static double interestRate = 0.02;
+    private static int idCounter = 1;
+    private int id;
+    private  double balance;
+
+    public BankAccount() {
+        this.id = idCounter;
+        idCounter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+    public void deposit (double amount){
+        this.balance += amount;
+
+    }
+    public double getInterestRate(int year){
+        return  BankAccount.interestRate * year * this.balance;
+    }
 }
--- DefiningClasses/BankAccount/Main.java
+++ DefiningClasses/BankAccount/Main.java
@@ -1,9 +1,58 @@
 package Advanced.DefiningClasses.BankAccount;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        Map<Integer, BankAccount> bankAccountMap = new HashMap<>();
+
+        while (!input.equals("End")) {
+            String output = null;
+            String[] commands = input.split("\\s+");
+            switch (commands[0]) {
+
+                case "Create":
+                    BankAccount bankAccount = new BankAccount();
+                    bankAccountMap.put(bankAccount.getId(), bankAccount);
+                    output = "Account ID" + bankAccount.getId() + " created";
+                    break;
+                case "Deposit": {
+                    int id = Integer.parseInt(commands[1]);
+                    int amount = Integer.parseInt(commands[2]);
+                    if (bankAccountMap.containsKey(id)) {
+                        bankAccount = bankAccountMap.get(id);  //!!!!!!!
+                        bankAccount.deposit(amount);
+                        output = "Deposited " + amount + " to " + "ID" + id;
+                    } else {
+                        output = "Account does not exist";
+                    }
+                    break;
+                }
+                case "SetInterest":
+                    double interestRate = Double.parseDouble(commands[1]);
+                    BankAccount.setInterestRate(interestRate);//!!!!!
+                    break;
+                case "GetInterest":
+                    int id = Integer.parseInt(commands[1]);
+                    int year = Integer.parseInt(commands[2]);
+                    if (bankAccountMap.containsKey(id)) {
+
+                        output = String.format("%.2f", bankAccountMap.get(id).getInterestRate(year));
+                    } else {
+                        output = "Account does not exist";
+                    }
+                    break;
+            }
+            if (output != null) {
+                System.out.println(output);
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
