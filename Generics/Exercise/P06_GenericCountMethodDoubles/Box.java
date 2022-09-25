7fb94a6e51c1497115c00e8455a489d69cdec4de
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-09-25 22:05:23 +0300

    Advanced

---
+++ Generics/Exercise/P06_GenericCountMethodDoubles/Box.java
@@ -0,0 +1,44 @@
+package Generics.Exercise.P06_GenericCountMethodDoubles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Comparable<T>> {
+    private List<T> list;
+
+    public Box() {
+        list = new ArrayList<>();
+    }
+
+    public void add(T input) {
+        this.list.add(input);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        if (firstIndex >= 0 && secondIndex <= this.list.size() - 1) {
+            T firstElement = this.list.get(firstIndex);
+            T secondElement = this.list.get(secondIndex);
+            this.list.set(firstIndex, secondElement);
+            this.list.set(secondIndex, firstElement);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (T element : this.list) {
+            builder.append(String.format("%s: %s%n", element.getClass().getName(), element));
+        }
+        return builder.toString();
+    }
+
+    public int compareTo(T compare) {
+        int counter = 0;
+        for (T element : this.list) {
+            if (element.compareTo(compare) > 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ Generics/Exercise/P06_GenericCountMethodDoubles/Main.java
@@ -0,0 +1,21 @@
+package Generics.Exercise.P06_GenericCountMethodDoubles;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Double> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            double input = Double.parseDouble(scanner.nextLine());
+
+            box.add(input);
+        }
+
+       double compare = Double.parseDouble(scanner.nextLine());
+      int counter = box.compareTo(compare);
+        System.out.println(counter);
+    }
+}
