6fd0086185c995733f762b23888fb428176c87ff
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-09-25 22:04:22 +0300

    Advanced

---
+++ Generics/Exercise/P04_GenericSwapMethodInteger/Main.java
@@ -0,0 +1,23 @@
+package Generics.Exercise.P04_GenericSwapMethodInteger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(scanner.nextLine());
+
+            box.add(input);
+        }
+
+        int firstIndex = scanner.nextInt();
+        int secondIndex = scanner.nextInt();
+        box.swap(firstIndex,secondIndex);
+
+        System.out.println(box);
+    }
+}
