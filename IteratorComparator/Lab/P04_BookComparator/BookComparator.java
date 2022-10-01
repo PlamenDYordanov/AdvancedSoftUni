9ae27c7b6d9e072c276c40ea495450ee08db7fae
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-10-01 14:32:04 +0300

    Advanced

---
+++ IteratorComparator/Lab/P04_BookComparator/BookComparator.java
@@ -0,0 +1,14 @@
+package P04_BookComparator;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book bookOne, Book bookTwo) {
+        int result = bookOne.getTitle().compareTo(bookTwo.getTitle());
+        if (result == 0){
+            result = Integer.compare(bookOne.getYear(), bookTwo.getYear());
+        }
+        return result;
+    }
+}
